723c7e2592efb6ae57f0bf2a35ba0ef64fc80703
Phillip Madsen <58571584+ap-coder@users.noreply.github.com>
2023-10-12 19:12:30 -0600

    Update cleaned-setup.sh

--- cleaned-setup.sh
+++ cleaned-setup.sh
@@ -1,21 +1,39 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
+set -euo
+
+# Ensure the script is run as ubuntu user
+if [ "$(whoami)" != "ubuntu" ]; then
+    echo "Script must be run as user: ubuntu"
+    exit 1
+fi
+
+# Navigate to the ubuntu user home directory
+cd /home/ubuntu || exit
+
+
 command_exists() {
     command -v "$1" >/dev/null 2>&1;
 }
 
+# Ensure sudo is used for commands needing root access
 sudo apt update -y
- 
+
 if ! command_exists apache2; then
     sudo add-apt-repository -y ppa:ondrej/apache2
     sudo apt update
 fi
 
-sudo apt install -y --no-install-recommends libmagickwand-dev apache2 libapache2-mod-php imagemagick ghostscript-x supervisor software-properties-common unzip zip redis-server
-git clone https://github.com/ap-coder/php-switch-scripts.git
-bash ./php-switch-scripts/setup.sh 
-bash ./php-switch-scripts/switch-to-php-8.1.sh
+# Using 'sudo -u ubuntu -H' for all commands related to the ubuntu user
+sudo -u ubuntu -H git clone https://github.com/ap-coder/php-switch-scripts.git /home/ubuntu/php-switch-scripts || { echo "Failed to clone repository"; exit 1; }
+sudo -u ubuntu -H bash /home/ubuntu/php-switch-scripts/setup.sh
+sudo -u ubuntu -H bash /home/ubuntu/php-switch-scripts/switch-to-php-8.1.sh
+
+# Install necessary packages
+sudo DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends libmagickwand-dev apache2 libapache2-mod-php imagemagick ghostscript-x supervisor software-properties-common unzip zip redis-server
 sudo a2enmod rewrite
 
+# Composer install
 if ! command_exists composer; then
     wget -O composer-setup.php https://getcomposer.org/installer
     sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
@@ -23,49 +41,52 @@ if ! command_exists composer; then
     sudo composer self-update
 fi
 
-mkdir -p ~/www/html/public
-ssh-keygen -t rsa -b 4096 -C "production@tyrant" -N "" -f ~/.ssh/id_rsa
+# SSH and directory setup for ubuntu user
+sudo -u ubuntu -H mkdir -p /home/ubuntu/www/html/public
 
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
+SSH_EMAIL="dev@mmi.io"
 
-cat <<EOD >> ~/.bashrc
-export PS1='\[\e]0;\h\a\] \[\e[0;32m\][\A]\[\e[0m\] \[\e[0;35m\]REMOTE:\h\[\e[0m\] \[\e[1;31m\][PROD]\[\e[0m\]\[\e[0m\] in \[\e[1;33m\]\w\[\e[0m\] $ 
-EOD
+if [ ! -f /home/ubuntu/.ssh/id_rsa ]; then
+    sudo -u ubuntu -H ssh-keygen -t rsa -b 4096 -C "${SSH_EMAIL}" -N "" -f /home/ubuntu/.ssh/id_rsa
+fi
 
-cat <<EOD >> ~/.bash_aliases
-# general
+# Start ssh-agent for ubuntu user
+sudo -u ubuntu -H bash -c 'eval $(ssh-agent -s) && ssh-add ~/.ssh/id_rsa'
+
+# Bashrc and alias setup for ubuntu user
+sudo -u ubuntu bash -c "cat <<'EOD' >> /home/ubuntu/.bashrc
+export PS1='\[\e]0;\h\a\] \[\e[0;32m\][\A]\[\e[0m\] \[\e[0;35m\]REMOTE:\h\[\e[0m\] \[\e[1;31m\][PROD]\[\e[0m\]\[\e[0m\] in \[\e[1;33m\]\w\[\e[0m\] \\$ '
+EOD"
+
+sudo -u ubuntu bash -c "cat <<'EOD' >> /home/ubuntu/.bash_aliases
 alias current='cd /var/www/html/current'
 alias history='cd /var/www/html/releases'
-alias taillogs="cd /var/www/html/current && tail -f -n250 storage/logs/laravel"
-alias tailsuper="cd /var/www/html/current && tail -f -n250 storage/logs/worker.log"
-# APACHE STUFF
+alias taillogs='cd /var/www/html/current && tail -f -n250 storage/logs/laravel'
+alias tailsuper='cd /var/www/html/current && tail -f -n250 storage/logs/worker.log'
 alias reload-apache='sudo systemctl reload apache2'
 alias restart-apache='sudo systemctl restart apache2'
 alias areload='sudo systemctl reload apache2'
 alias arestart='sudo systemctl restart apache2'
-# Supervisor Stuff
 alias restart-super='sudo supervisorctl restart all'
-# aliases for directory traversal
 alias c='clear'
 alias ..='cd ../'
 alias ...='cd ../../'
 alias ....='cd ../../../'
 alias .....='cd ../../../../'
-# Alter the ls command
 alias ls='ls -ac'
 alias lls='ls -lac'
-alias la="ls --color -lAGbh"
-alias ll="ls -lah --color" # List all, with human readable filesizes
-# Artisan aliases
+alias la='ls --color -lAGbh'
 alias art='php artisan'
-#composer
-alias cu="composer update"
-# Phillips
-alias tree="find . -print | sort -r | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
+alias cu='composer update'
+alias tree=\"find . -print | sort -r | sed -e 's;[^/]*/;|____;g;s;____|; |;g'\"
 alias rst='source ~/.bashrc'
 alias statusall='service --status-all'
-alias superstatus="sudo systemctl status supervisor"
-EOD
-
+alias superstatus='sudo systemctl status supervisor'
+EOD"
 sudo timedatectl set-timezone America/Denver
+sudo chown ubuntu:ubuntu /home/ubuntu/.bashrc
+sudo chown ubuntu:ubuntu /home/ubuntu/.bash_aliases
+sudo chown -R ubuntu:ubuntu /home/ubuntu/ /var/www/
+sudo chmod -R 755 /home/ubuntu/ /var/www/
+
+echo "The script ran successfully!"
